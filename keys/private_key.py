a8336a3307c2506a524f732f938f4bcb5bd32f0d
Matheus Sena <sena.matheus14@gmail.com>
2019-07-02 08:45:24 -0300

    PrivateKey decrypt now can return string just passing as_strign=True

--- keys/private_key.py
+++ keys/private_key.py
@@ -13,14 +13,27 @@ class PrivateKey(object):
         for data in text:
             yield pow(ord(data), self._d) % self._n
 
-    def decrypt(self, text):
+    def decrypt(self, text, as_string=False):
+
+        def return_not_as_string(text):
+            for data in text:
+                yield chr(pow(data, self._d) % self._n)
+
+        def return_as_string(text):
+            string = str()
+            for data in text:
+                string += chr(pow(data, self._d) % self._n)
+            return string
+
         try:
             getattr(text, '__iter__')
         except:
             raise NotAnIterableObject("text must to be an iterator")
         else:
-            for data in text:
-                yield chr(pow(data, self._d) % self._n)
-
+            if as_string:
+                return return_as_string(text)
+            else:
+                return return_not_as_string(text)
+            
     def __repr__(self):
         return 'PrivateKey(%s, %s)' % (self._d, self._n)
